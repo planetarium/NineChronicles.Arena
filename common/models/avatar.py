5ffcf2f0693a7c1ae65e8180f2664f01ae803b2c
hyeon <hyeon@planetariumhq.com>
2023-07-28 11:36:26 +0900

    Update model
    - Delete Avatar model
    - Create new avatar data for every season

--- common/models/avatar.py
+++ common/models/avatar.py
@@ -1,27 +1,20 @@
-from sqlalchemy import Text, Column, ForeignKey, Integer
+from sqlalchemy import Text, Column, ForeignKey, Integer, Index
 from sqlalchemy.orm import backref, relationship
 
 from common.const import ARENA_START_SCORE
 from common.models.base import Base, AutoIdMixin
 
 
-class Avatar(Base):
-    __tablename__ = "avatar"
+class ArenaInfo(AutoIdMixin, Base):
+    # Create new arena info for same avatar every season
+    __tablename__ = "arena_info"
 
-    addr = Column(Text, primary_key=True, nullable=False, index=True)
+    avatar_addr = Column(Text, nullable=False)
     agent_addr = Column(Text)
     level = Column(Integer, nullable=False)
     costume_armor_id = Column(Integer, nullable=False, default=10200000,
                               doc="Full costume or armor ID to draw profile image")
     title_id = Column(Integer, nullable=True)
-    # cp = Column(Integer)
-
-
-class ArenaInfo(AutoIdMixin, Base):
-    __tablename__ = "arena_info"
-
-    avatar_addr = Column(Text, ForeignKey("avatar.addr"), nullable=False)
-    avatar = relationship("Avatar", foreign_keys=[avatar_addr], backref=backref("arena_info", uselist=False))
     arena_id = Column(Integer, ForeignKey("arena.id"), nullable=False)
     arena = relationship("Arena", foreign_keys=[arena_id], backref=backref("participant_list"))
     # championship = Column(Integer, nullable=False)
@@ -34,6 +27,6 @@ class ArenaInfo(AutoIdMixin, Base):
     score = Column(Integer, nullable=False, default=ARENA_START_SCORE)
     # ranking = Column(Integer, nullable=False)
 
-    # __table_args__ = (
-    #     Index("avatar_arena_id_idx", "avatar_addr", "championship", "round"),
-    # )
+    __table_args__ = (
+        Index("avatar_arena_id_idx", "avatar_addr", "arena_id"),
+    )
