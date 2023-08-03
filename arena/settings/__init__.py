d21951cefadeb60d788836cfcc8aaf8c1ff87b99
hyeon <hyeon@planetariumhq.com>
2023-08-03 22:59:28 +0900

    Add settings for DB

--- arena/settings/__init__.py
+++ arena/settings/__init__.py
@@ -3,7 +3,10 @@ import os
 
 from starlette.config import Config
 
+from common.utils.aws import fetch_secrets
+
 stage = os.environ.get("STAGE", "local")
+db_password = None
 
 if not stage:
     logging.error("Config file not found")
@@ -15,8 +18,14 @@ if os.path.exists(os.path.join("arena", "settings", f"{stage}.py")):
     config = Config(environ=envs)
 else:
     config = Config(environ=os.environ)
+    db_password = fetch_secrets(os.environ.get("REGION_NAME"), os.environ.get("SECRET_ARN"))["password"]
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 LOG_LEVEL = logging.getLevelName(config("LOG_LEVEL", default="INFO"))
+
+DB_URI = config("DB_URI")
+if db_password is not None:
+    DB_URI = DB_URI.replace("[DB_PASSWORD]", db_password)
+DB_ECHO = config("DB_ECHO", cast=bool, default=False)
 
 REGION_NAME = config("REGION_NAME")
