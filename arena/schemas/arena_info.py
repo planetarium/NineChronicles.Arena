1a4363316b3f46076d716daf2fcaf1c88373d0a7
hyeon <hyeon@planetariumhq.com>
2023-08-08 13:44:07 +0900

    Update schema to provide CP

--- arena/schemas/arena_info.py
+++ arena/schemas/arena_info.py
@@ -21,11 +21,14 @@ class ArenaParticipantSchema(BaseSchema):
     avatar_addr: str
     name: str
     level: int
+    cp: int
     costume_id: int = 10200000
     title_id: Optional[int] = None
     score: int
     rank: int = 0
-    expect_win_score: int = 0
+    # Client will calculate these values
+    # expect_win_score: int = 0
+    # expect_lose_score: int = 0
 
     class Config:
         from_attributes = True
